a8c1e8dc8dce5a1e66cc7d4ea2a009d41d3a2fa2
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2022-12-21 14:39:57 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+         Warrior warrior = new Warrior();
+
+         Magic magic = new Magic();
+
+         Medic medic = new Medic();
+
+         Hero [] heroes = {warrior, magic, medic};
+        for (int i = 0; i < heroes.length; i++) {
+            heroes[i].applySuperAbility();
+
+        }
+
+
+                }
+            }
+
